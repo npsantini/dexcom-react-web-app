8c4a79e7d5da85475b5b573a296135bd9ead1102
Nick Santini <npsantini@gmail.com>
2023-02-23 12:00:54 -0600

    Initial commit for login page

---
+++ pages/dexcom-login.js
@@ -0,0 +1,63 @@
+import Link from 'next/link';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function Login() {
+    return (
+      <div className={styles.container}>
+        <Head>
+          <title>Dexcom Login</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+  
+        <main>
+          <h1 className={styles.title}>
+            Dexcom Login
+          </h1>
+  
+          <p className={styles.description}>
+            Get started by logging in.
+            <br/>
+            <Link href="/">Home Page</Link>
+          </p>
+        </main>
+  
+        <footer>
+            <p>Dexcom React.js and Next.js app.</p>
+        </footer>
+  
+        <style jsx>{`
+          main {
+            padding: 5rem 0;
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+          }
+          footer {
+            width: 100%;
+            height: 100px;
+            border-top: 1px solid #eaeaea;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+        `}</style>
+  
+        <style jsx global>{`
+          html,
+          body {
+            padding: 0;
+            margin: 0;
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+              Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+              sans-serif;
+          }
+          * {
+            box-sizing: border-box;
+          }
+        `}</style>
+      </div>
+    )
+  }
